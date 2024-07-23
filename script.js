3dd4eef56ebb2b6e5324d5f3241ee40363867a57
DeanHdz <dean2103@gmail.com>
2024-07-23 03:39:52 -0600

    small header adjustment animation, responsiveness still pending

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 window.addEventListener('scroll', () => {
     const header = document.querySelector('header');
+    const Logo = document.getElementById('Logo');
     if (window.scrollY > 50) {
-        header.classList.add('scrolled');
+        header.classList.add('scrolled-bg');
+        Logo.classList.add('scrolled-logo');
     } else {
-        header.classList.remove('scrolled');
+        header.classList.remove('scrolled-bg');
+        Logo.classList.remove('scrolled-logo');
     }
 });
