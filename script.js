378b743ce4830500ef760e455c4c06ba86b19f72
DeanHdz <dean2103@gmail.com>
2024-07-24 15:09:30 -0600

    Fondo responsivo

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 window.addEventListener('scroll', () => {
     const header = document.querySelector('header');
     const Logo = document.getElementById('Logo');
+    const nav = document.querySelector('nav');
     if (window.scrollY > 50) {
         header.classList.add('scrolled-bg');
         Logo.classList.add('scrolled-logo');
+        nav.classList.add('scrolled-nav');
     } else {
         header.classList.remove('scrolled-bg');
         Logo.classList.remove('scrolled-logo');
+        nav.classList.remove('scrolled-nav');
     }
 });
