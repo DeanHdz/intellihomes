ed0676f1c9f8111db2bb92e6e2b6f39961844a86
DeanHdz <dean2103@gmail.com>
2024-07-24 19:49:25 -0600

    Animated welcome

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+//Cambiar imagen de fondo de "background" constantemente, para dar efecto de animacion
+const background = document.getElementById('background');
+background.style.backgroundImage = 'url(assets/images/16x9_1.jpg)';
+const images = ['assets/images/16x9_1.jpg', 'assets/images/16x9_2.png', 'assets/images/16x9_3.png', 'assets/images/16x9_4.png'];
+let i = 1;
+setInterval(() => {
+    background.style.backgroundImage = `url(${images[i]})`;
+    if(i == images.length - 1) {
+        i = 0;
+    } else {
+        i++;
+    }
+    console.log(i);
+}, 5500);
+
+//Manejar visualizacion del header al hacer scroll hacia abajo
 window.addEventListener('scroll', () => {
     const header = document.querySelector('header');
     const Logo = document.getElementById('Logo');
