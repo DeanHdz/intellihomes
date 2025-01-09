47a2a683e69034ead22ea02990fa4e590773189d
DeanHdz <dean2103@gmail.com>
2025-01-09 16:00:08 -0600

    Rediseño de seccion paquetes y animacion de aparición de marcas

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
+////// FUNCIONES PARA MANEJAR LA INTERACCION DEL USUARIO CON LA PAGINA WEB //////
+
+//Manejo del sidebar en vista celular
+function toggleSidebar() {
+    const aside = document.querySelector('aside');
+    const overlay = document.querySelector('#overlay');
+    aside.classList.toggle('show');
+    overlay.classList.toggle('show');
+}
+
+
+
+////// MANEJO AUTOMATICO DE LA PAGINA WEB //////
+
 //Cambiar imagen de fondo de "background" constantemente, para dar efecto de animacion
 const images = document.querySelectorAll('.background-image');
 let currentIndex = 0;
-// Cambiar imagen cada 5.5 segundos
 setInterval(() => {
-// Guardar el índice de la imagen actual
 let currentImageIndex = currentIndex;
-// Calcular el siguiente índice
 currentIndex = (currentIndex + 1) % images.length;
-// Mostrar la nueva imagen
 images[currentIndex].classList.add('active');
-// Ocultar la imagen actual
 images[currentImageIndex].classList.remove('active');
 }, 10000);
+
 
 //Manejar visualizacion del header al hacer scroll hacia abajo
 window.addEventListener('scroll', () => {
@@ -31,21 +41,21 @@ window.addEventListener('scroll', () => {
 });
 
 
-//Manejar display del navbar cuando se reajuste el tamaño de la ventana
-window.addEventListener('resize', () => {
-    const nav = document.querySelector('nav');
-    if(window.innerWidth >= 768){
-        nav.classList.remove('show');
-    }
+//Manejar visualizacion de las marcas al hacer scroll hacia abajo
+document.addEventListener("DOMContentLoaded", () => {
+    const brands = document.querySelectorAll(".brand");
+    
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+        });
+    }, { threshold: 0.2 }); // Cuando el 20% de la imagen sea visible
+    
+    brands.forEach((brand) => observer.observe(brand));
 });
 
-//Manejo del sidebar en vista celular
-function toggleSidebar() {
-    const aside = document.querySelector('aside');
-    const overlay = document.querySelector('#overlay');
-    aside.classList.toggle('show');
-    overlay.classList.toggle('show');
-}
 
 //Cerrar sidebar al hacer click en el overlay (Fondo oscuro)
 document.querySelector('#overlay').addEventListener('click', () => {
@@ -53,11 +63,15 @@ document.querySelector('#overlay').addEventListener('click', () => {
     document.querySelector('#overlay').classList.remove('show');
 });
 
+
 //Manejo de la visualizacion de la fecha actual en el footer
 document.getElementById("currentYear").textContent = new Date().getFullYear();
 
 
-//NO UTILIZADO
+
+
+
+////////   NO UTILIZADO   ////////
 
 //Manejo del navbar en vista celular
 function toggleNav() {
@@ -69,3 +83,10 @@ function toggleNav() {
     }
 }
 
+//Manejar display del navbar cuando se reajuste el tamaño de la ventana
+/*window.addEventListener('resize', () => {
+    const nav = document.querySelector('nav');
+    if(window.innerWidth >= 768){
+        nav.classList.remove('show');
+    }
+});*/
